e0fd2aa15476ac172277bedd67c71112aa69b768
nithishmaile <132052881+nithishmaile@users.noreply.github.com>
2025-11-26 15:33:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-// write js code if required
+// write js code if required\
+const flip=document.getElementById("flip");
+
+flip.addEventListener("mouseover",()=>{
+	flip.src="https://live.staticflickr.com/8138/29099429912_0659caf55b_b.jpg";
+})
+
+flip.addEventListener("mouseout",()=>{
+	flip.src="https://images.pexels.com/photos/6785279/pexels-photo-6785279.jpeg";
+})
